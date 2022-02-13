472db62f881b8efb76aab4d8ee13c9ff801abb2a
Patryk Michalik <contact@patrykmichalik.com>
2022-02-13 11:39:28 +0100

    Remove unneeded fields from `Preference` & update dependencies
    Co-authored-by: Patryk Goworowski <patrykgoworowski@gmail.com>

--- preference-manager/src/main/kotlin/com/patrykmichalik/preferencemanager/Preference.kt
+++ preference-manager/src/main/kotlin/com/patrykmichalik/preferencemanager/Preference.kt
@@ -1,16 +1,9 @@
 package com.patrykmichalik.preferencemanager
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import kotlinx.coroutines.flow.Flow
 
 interface Preference <C, S> {
-    val parse: (S) -> C
-    val save: (C) -> S
-    val onSet: () -> Unit
     val defaultValue: C
-    val key: Preferences.Key<S>
-    val preferencesDataStore: DataStore<Preferences>
 
     fun get(): Flow<C>
     suspend fun set(value: C)
--- preference-manager/src/main/kotlin/com/patrykmichalik/preferencemanager/PreferenceImpl.kt
+++ preference-manager/src/main/kotlin/com/patrykmichalik/preferencemanager/PreferenceImpl.kt
@@ -3,17 +3,15 @@ package com.patrykmichalik.preferencemanager
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 
 class PreferenceImpl<C, S>(
-    override val parse: (S) -> C,
-    override val save: (C) -> S,
-    override val onSet: () -> Unit,
+    val parse: (S) -> C,
+    val save: (C) -> S,
+    val onSet: () -> Unit,
     override val defaultValue: C,
-    override val key: Preferences.Key<S>,
-    override val preferencesDataStore: DataStore<Preferences>,
+    val key: Preferences.Key<S>,
+    val preferencesDataStore: DataStore<Preferences>,
 ): Preference<C, S> {
 
     private fun S?.parsedOrDefault() = this?.let { parse(it) } ?: defaultValue
